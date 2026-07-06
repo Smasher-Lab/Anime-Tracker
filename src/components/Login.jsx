42be0e6f6e2c86cdc929f4bcdd67a422048c5297
Smasher-Lab <mukkulalith@gmail.com>
2026-07-06 20:14:05 +0530

    added API URL

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import API_URL from '../config';
 
 function Login() {
   const [username, setUsername] = useState('');
